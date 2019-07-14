3958c6c3625b09a5318a306a1550aa11b4426276
Jordan Cantrell <jordan.cantrell@mail.com>
2019-07-13 21:29:45 -0700

    generic

--- src/tests/unit/testmorsel.cpp
+++ src/tests/unit/testmorsel.cpp
@@ -2,22 +2,37 @@
 #include "../../model/cpu/morsel.h"
 class TestMorsel : public Morsel
 {
+  private:
+    dynamic_bitset<> getBitset()
+    {
+      return bs;
+    }
 	public:
-	bool testCount()
-	{
-		dynamic_bitset<> tbs(64,0ul);
-		return tbs.count() == count();
-	}
+  TestMorsel() : Morsel() {}
+  TestMorsel(dynamic_bitset<> in) : Morsel(in) {}
+  bool testCount()
+  {
+    dynamic_bitset<> tbs(64,0ul);
+    return tbs.count() == count();
+  }
+  bool testMorsel_bitset()
+  {
+    dynamic_bitset<> tbs(64,0ul);
+    TestMorsel tm(tbs);
+    return tbs == tm.getBitset();
+  }
+
+  void runAllTests()
+  {
+    TestMorsel tm;
+	  std::cout << "Test " << (tm.testCount() ? "passed" : "failed") << endl;
+	  std::cout << "Test " << (tm.testMorsel_bitset() ? "passed" : "failed") << endl;
+  }
 };
 
 int main()
 {
 	TestMorsel tm;
-	if (tm.testCount())
-	{
-		std::cout << "Test passed" << endl;
-	} else {
-		std::cout << "Test failed" << endl;
-	}
+  tm.runAllTests();
 	return 0;
 }
